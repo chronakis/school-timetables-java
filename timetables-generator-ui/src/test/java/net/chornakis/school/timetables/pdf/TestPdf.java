946049d41650a70bf9b1e03c037988335ff66abf
Ioannis Chronakis <ioannis.chronakis@deontics.com>
2023-03-08 14:48:06 +0000

    Tested a usable pdf format

--- timetables-generator-ui/src/test/java/net/chornakis/school/timetables/pdf/TestPdf.java
+++ timetables-generator-ui/src/test/java/net/chornakis/school/timetables/pdf/TestPdf.java
@@ -6,19 +6,25 @@ import java.io.IOException;
 import org.junit.Test;
 
 import com.itextpdf.io.font.FontConstants;
-import com.itextpdf.io.font.FontProgram;
-import com.itextpdf.io.font.FontProgramFactory;
 import com.itextpdf.io.font.PdfEncodings;
+import com.itextpdf.kernel.color.Color;
 import com.itextpdf.kernel.font.PdfFont;
 import com.itextpdf.kernel.font.PdfFontFactory;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.kernel.pdf.canvas.PdfCanvasConstants;
 import com.itextpdf.layout.Document;
+import com.itextpdf.layout.Style;
+import com.itextpdf.layout.border.Border;
+import com.itextpdf.layout.border.SolidBorder;
 import com.itextpdf.layout.element.AreaBreak;
+import com.itextpdf.layout.element.Cell;
 import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Table;
 import com.itextpdf.layout.element.Text;
 import com.itextpdf.layout.property.AreaBreakType;
+import com.itextpdf.layout.property.TextAlignment;
+import com.itextpdf.layout.property.UnitValue;
 
 public class TestPdf {
 
@@ -70,6 +76,71 @@ public class TestPdf {
 		doc.close();
 	}
 	
+	@Test
+	public void testTables() throws IOException {
+		String fontFile = "src/main/resources/fonts/ChivoMono-VariableFont_wght.ttf";
+        //PdfFont questFont = PdfFontFactory.createFont(fontFile, PdfEncodings.WINANSI, true, true);
+        PdfFont questFont = PdfFontFactory.createFont(FontConstants.COURIER);
+		PdfFont titleFont = PdfFontFactory.createFont(FontConstants.HELVETICA);
+
+		Style titleStyle = new Style();
+		titleStyle
+			.setFont(titleFont)
+			.setFontSize(24.0f)
+			.setPaddings(0f, 8.0f, 0f, 8.0f)
+			.setBorderBottom(new SolidBorder(1.0f))
+			;
+		Style footerStyle = new Style();
+		footerStyle
+			.setFont(titleFont)
+			.setFontSize(12.0f)
+			.setPaddingTop(4.0f)
+			.setBorderTop(new SolidBorder(0.5f))
+			.setTextAlignment(TextAlignment.RIGHT)
+			;
+		
+		Style questStyle = new Style();
+		questStyle.setFont(questFont).setFontSize(22.0f);
+		
+		String dest = "testTables.pdf";
+		PdfWriter writer = new PdfWriter(dest);
+
+		PdfDocument pdfDoc = new PdfDocument(writer);
+		Document doc = new Document(pdfDoc);
+
+
+		Text title = new Text("5, 6 & 7 timetables. 20 Questions.");
+		doc.add(new Paragraph(title).addStyle(titleStyle));
+		
+		// Build the questions
+		Table table = new Table(UnitValue.createPercentArray(new float [] {50.0f, 50.0f}))
+				.addStyle(questStyle)
+				//.setBorder(Border.NO_BORDER)
+				.useAllAvailableWidth()
+				.setMargins(30, 20, 0, 20)
+				;
+		
+		for (int i = 0; i < 24; i++) {
+			int num = (int) (10 * Math.random()) + 1;
+			String leading = String.format("%3d", num).replace(" ", "\u00A0");
+			String questText = String.format("%s x%3d = %3d", leading, i, num * i);
+			//Text text = new Text(questText).addStyle(questStyle);
+			// doc.add(new Paragraph().add(dot).add(text));
+			Cell cell = new Cell()
+					.setTextAlignment(i % 2 == 0 ? TextAlignment.LEFT : TextAlignment.RIGHT)
+					.setMargins(0, 0, 22.0f, 0)
+					.setBorder(Border.NO_BORDER)
+					.add(questText)
+					;
+			table.addCell(cell);
+		}
+		
+		doc.add(table);
+		
+		doc.add(new Paragraph("Page 1").addStyle(footerStyle));
+		doc.close();
+	}
+
 	private Text textWithWidth(String text, PdfFont font, float width) {
 		Text result = new Text(text)
 		        .setFont(font)
